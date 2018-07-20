fc03e2d2dfeb4199342803793c523699d125ba1e
Dillon Huff <dillonhuff@gmail.com>
2018-07-20 02:34:02 -0700

    Basic accumulation test working

--- pe.v
+++ pe.v
@@ -1,17 +1,22 @@
 module pe(input clk,
-          input        rst,
+          input         rst,
 
-          input [31:0] a,
-          input [31:0] b,
+          input [31:0]  a,
+          input [31:0]  b,
+
+          input input_valid,
 
           output [31:0] dbg_c);
 
    reg [31:0]   c;
    
    always @(posedge clk or negedge rst) begin
+      // $display("a = ", a);
+      // $display("b = ", b);      
+
       if (!rst) begin
          c <= 0;
-      end else begin
+      end else if (input_valid) begin
          c <= c + a*b;
       end
    end
--- pe_main.cpp
+++ pe_main.cpp
@@ -1,8 +1,12 @@
 #include "Vpe.h"
 #include "verilated.h"
 
-#define POSEDGE(top, signal) (top)->signal = 0; (top)->signal = 1;
-#define NEGEDGE(top, signal) (top)->signal = 1; (top)->signal = 0;
+#define POSEDGE(top, signal) (top)->signal = 0; (top)->signal = 1; (top)->eval();
+#define NEGEDGE(top, signal) (top)->signal = 1; (top)->signal = 0; (top)->eval();
+
+#include <iostream>
+
+using namespace std;
 
 void test_rst() {
   Vpe* top = new Vpe();
@@ -15,6 +19,32 @@ void test_rst() {
   delete top;
 }
 
+void test_accumulate() {
+
+  Vpe* top = new Vpe();
+  top->clk = 0;
+  top->rst = 0;
+
+  POSEDGE(top, rst);
+  NEGEDGE(top, rst);
+  POSEDGE(top, rst);
+
+  assert(top->dbg_c == 0);
+
+  top->input_valid = 1;
+  top->a = 10;
+  top->b = 3;
+
+  POSEDGE(top, clk);
+
+  cout << "c = " << top->dbg_c << endl;
+
+  assert(top->dbg_c == 30);
+  
+  delete top;
+}
+
 int main() {
   test_rst();
+  test_accumulate();
 }
